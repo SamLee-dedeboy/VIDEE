1f2ce48f0b6cdd158333ce0ca4f8955a31f92786
SamLee-dedeboy <samytleezzz@gmail.com>
2025-02-10 18:20:35 -0800

    add animation to automatically centering of new nodes

--- src/renderer/dag.ts
+++ src/renderer/dag.ts
@@ -14,7 +14,7 @@ export class DAG {
     line: any
     selection_card: string
     selection_container: string
-    coordinate_as_dict: any
+    bbox_dict: any
     dag: any
     zoom: any
     handleClick: Function
@@ -25,11 +25,11 @@ export class DAG {
         this.dag = undefined;
         this.zoom = d3.zoom().scaleExtent([0.1, 32])
         .on("zoom", (e) => this.zoomed(e, this))
-        .on("end", () => document.querySelectorAll(this.selection_card).forEach(div => div.style.transitionDuration = "0.5s"))
+        .on("end", () => document.querySelectorAll(this.selection_card).forEach((div: any) => div.style.transitionDuration = "0.5s"))
 
         this.selection_card = selection_card
         this.selection_container = selection_container
-        this.coordinate_as_dict = {}
+        this.bbox_dict = {}
         this.handleClick = () => {}
     }
     init (handleClick=() => {}) {
@@ -37,7 +37,8 @@ export class DAG {
         svg.append("g").attr("class", "links");
         svg.append("g").attr("class", "nodes");
         svg.append("g").attr("class", "bubbles");
-        svg.call(this.zoom).call(this.zoom.transform, d3.zoomIdentity);
+        svg.call(this.zoom)
+        // .call(this.zoom.transform, d3.zoomIdentity);
         this.handleClick = handleClick
 
         console.log("init done")
@@ -59,66 +60,49 @@ export class DAG {
         const rect_size: [number, number] = this.nodeRadius
         const layout = d3_dag
         .sugiyama()
+        .coord(d3_dag.coordQuad())
         // .grid()
         // .lane(d3_dag.laneOpt())
         // .coord(d3_dag.coordGreedy())
         // .nodeSize(this.nodeRadius)
-        .nodeSize(rect_size)
+        .nodeSize((node: any) => {
+            return [(node.data.bbox?.width || rect_size[0])  , (node.data.bbox?.height || rect_size[1])]
+        })
+        // .nodeSize(rect_size)
         .gap([50, 50])
         // .tweaks([d3_dag.tweakSize({width: max_width, height: max_height})])
         // .tweaks([d3_dag.tweakShape(rect_size, d3_dag.shapeRect)])
         // .tweaks([d3_dag.tweakShape(rect_size, d3_dag.shapeRect), d3_dag.tweakSize({width: max_width, height: max_height})])
 
         const { width, height } = layout(this.dag);
-        const translation_scaling = [Math.max(1, max_width / width), 1.1*Math.max(1, max_height / height)]
+        // const translation_scaling = [Math.max(1, max_width / width), 1.1*Math.max(1, max_height / height)]
+        const translation_scaling = [1, 1]
         console.log({width, height, max_width, max_height})
 
-        const vertical = true;
-        this.coordinate_as_dict = Array.from(this.dag.nodes()).reduce((acc: any, d: any) => {
-            acc[d.data.id] = {x: d.x * translation_scaling[0], y: d.y * translation_scaling[1]};
+        this.bbox_dict = Array.from(this.dag.nodes()).reduce((acc: any, d: any) => {
+            acc[d.data.id] = {x: d.x * translation_scaling[0], y: d.y * translation_scaling[1], width: d.data.bbox?.width || rect_size[0], height: d.data.bbox?.height || rect_size[1]};
             return acc;
         }, {})
 
       // position nodes
       d3.selectAll(this.selection_card)
-        .style("left", function() {
+        .style("left", function() { 
             const id = this.dataset.id
-            const transform = d3.zoomTransform(svg.node())
-            if(this.style.transform !== "") {
-                return self.coordinate_as_dict[id].x - this.getBoundingClientRect().width / 2 / (transform.k === undefined? 1 : transform.k) + "px"
-            } else {
-                return self.coordinate_as_dict[id].x - this.getBoundingClientRect().width / 2 + "px"
-            }
-            // return coordinate_as_dict[id].x * (max_width / width) + "px";
-            return vertical?
-            self.coordinate_as_dict[id].x * (max_width / width) + "px":
-            self.coordinate_as_dict[id].y * (max_width / height) + "px"
-
+            return (self.bbox_dict[id].x - self.bbox_dict[id].width / 2) + "px"
         })
         .style("top", function() {
             const id = this.dataset.id
-            const transform = d3.zoomTransform(svg.node())
-            if(this.style.transform !== "") {
-                return self.coordinate_as_dict[id].y - this.getBoundingClientRect().height/ 2 / (transform.k === undefined? 1 : transform.k) + "px"
-            } else {
-                return self.coordinate_as_dict[id].y - this.getBoundingClientRect().height / 2 + "px"
-            }
-            return self.coordinate_as_dict[id].y * (max_height / height) + "px"
-            return vertical ? 
-            self.coordinate_as_dict[id].y * (max_height / height) + "px":
-            self.coordinate_as_dict[id].x * (max_height / width) + "px"
+            return (self.bbox_dict[id].y - self.bbox_dict[id].height / 2)   + "px"
         })
         .each(function() {
             applyTransform(this, d3.zoomTransform(svg.node()))
             this.style.transitionDuration = "0.5s"
         })
-        // left: 376px;
-//   top: 635.5px;
-//   transform: translate(-684px, -814.5px) scale(0.5) translate(684px, 814.5px) translate(595px, 126px)
 
+        let new_nodes: any[] = []
         svg.select("g.nodes")
-        // .selectAll("rect.node")
-            .data(Object.keys(self.coordinate_as_dict))
+        .selectAll("rect.node")
+            .data(Object.keys(self.bbox_dict), (d) => d)
             .join(
                 enter => enter.append("rect").attr("class", "node")
                         .attr("width",  0)
@@ -129,91 +113,82 @@ export class DAG {
                         .attr("rx", 10)
                         .attr("cursor", "pointer")
                         .attr("x", function(d) {
-                            return self.coordinate_as_dict[d].x - rect_size[0]/2
-                            // return coordinate_as_dict[d].x * (max_width / width) - rect_size[0]
+                            return (self.bbox_dict[d].x - self.bbox_dict[d].width/2) 
                         })
                         .attr("y", function(d) {
-                            return self.coordinate_as_dict[d].y - rect_size[1]/2
-                            // return coordinate_as_dict[d].y * (max_height / height) - rect_size[1]
+                            return (self.bbox_dict[d].y - self.bbox_dict[d].height/2)
                         })
-                        // .attr("x", ({ x }) => x * (max_width / width) - this.nodeRadius[0])
-                        // .attr("y", ({ y }) => y * (max_height / height) - this.nodeRadius[1])
                         .on("click", (_, d) => {
                             // this.handleClick(d)
                             d3.selectAll(self.selection_card)
                             .nodes()
                             .filter((div_data) => div_data.dataset.id === d)[0].style.visibility = "visible"
                         })
-                        .transition().duration(300)
-                        .attr("width", rect_size[0])
-                        .attr("height", rect_size[1])
+                        // .transition().duration(300)
+                        // .attr("width", (d) => self.bbox_dict[d].width)
+                        // .attr("height", (d) => self.bbox_dict[d].height)
+                        .each(function(d) {
+                            new_nodes.push(d)
+                        })
+                        // .attr("width", rect_size[0])
+                        // .attr("height", rect_size[1])
                         ,
                 update => update.transition().duration(500)
                         .attr("x", function(d) {
-                            return self.coordinate_as_dict[d].x - rect_size[0]/2
-                            // return coordinate_as_dict[d].x * (max_width / width) - rect_size[0]
+                            return (self.bbox_dict[d].x - self.bbox_dict[d].width/2) / d3.zoomTransform(svg.node()).k
+                            return self.bbox_dict[d].x - self.bbox_dict[d].width/2
+                            return self.bbox_dict[d].x - rect_size[0]/2
+                            // return bbox_dict[d].x * (max_width / width) - rect_size[0]
                         })
                         .attr("y", function(d) {
-                            return self.coordinate_as_dict[d].y - rect_size[1]/2
-                            // return coordinate_as_dict[d].y * (max_height / height) - rect_size[1]
+                            return (self.bbox_dict[d].y - self.bbox_dict[d].height/2) / d3.zoomTransform(svg.node()).k
+                            return self.bbox_dict[d].y - self.bbox_dict[d].height/2
+                            return self.bbox_dict[d].y - rect_size[1]/2
+                            // return bbox_dict[d].y * (max_height / height) - rect_size[1]
                         }),
                 exit => exit.transition().duration(300)
                         .attr("width",  0)
                         .attr("height",  0)
                         .remove()
-            )
+                )
 
         // plot edges
-
         this.update_links(translation_scaling)
-        // plot edges between decomposed tasks
-        const expansion_links = data.filter(d => expanded_nodes.includes(d.id)).map(d => {
-            const first_child_coord = self.coordinate_as_dict[d.sub_tasks?.[0].id]
-            console.log(d, first_child_coord)
-            const this_coord = self.coordinate_as_dict[d.id]
-            const link_coords = vertical?
-            [
-                [this_coord.x * (max_width / width), this_coord.y * (max_height / height)],
-                [first_child_coord.x * (max_width / width), first_child_coord.y * (max_height / height)]
-            ]:
-            [
-                [this_coord.y * (max_width / height), this_coord.x * (max_height / width)],
-                [first_child_coord.y * (max_width / height), first_child_coord.x * (max_height / width)]
+
+        // translate to make new nodes in the center
+        if(new_nodes.length !== 0) {
+            const new_nodes_bboxes = new_nodes.map(d => self.bbox_dict[d])
+            const new_nodes_center = [
+                d3.mean(new_nodes_bboxes.map(d => d.x)),
+                d3.mean(new_nodes_bboxes.map(d => d.y))
             ]
-            return link_coords
-        })
-        // svg.select("g.links")
-        // .selectAll("path.expansion-link")
-        // .data(expansion_links)
-        // .join("path")
-        // .attr("class", "expansion-link")
-        // .attr("d", (d) => this.line(d))
-        // .attr("fill", "none")
-        // .attr("stroke-width", 3)
-        // .attr("stroke", "gray")
-        // .attr("stroke-dasharray", "5,5")
+            // move the current zoom to the center of the new nodes
+            svg.transition().duration(500).delay(0).call(this.zoom.translateTo, new_nodes_center[0], new_nodes_center[1])
 
-        // add bubbles
-        return
-        const bubble_group = svg.select("g.bubbles")
-        const bubble_data = Object.groupBy(data, d => getGroup(d))
-        bubble_group.selectAll("path.dag-wrapper").remove()
-        Object.entries(bubble_data).forEach(([group, nodes]) => {
-            const points = dag_to_screen_coord(nodes?.map(d => 
-                [coordinate_as_dict[d.id].x, coordinate_as_dict[d.id].y]
-            ) || [], max_width, max_height, width, height, vertical)
-            console.log({nodes, points, coordinate_as_dict})
-            const bubble_path = create_bubble_path(points, 1.3 * self.nodeRadius) 
-            bubble_group.append("path")
-                .attr("class", "dag-wrapper").attr("d", bubble_path)
-                .attr("fill", "lightgreen")
-                .attr("opacity", 0.2)
-        })
+        }
 
+        // plot edges between decomposed tasks
+        // const expansion_links = data.filter(d => expanded_nodes.includes(d.id)).map(d => {
+        //     const first_child_coord = self.bbox_dict[d.sub_tasks?.[0].id]
+        //     console.log(d, first_child_coord)
+        //     const this_coord = self.bbox_dict[d.id]
+        //     const link_coords = vertical?
+        //     [
+        //         [this_coord.x * (max_width / width), this_coord.y * (max_height / height)],
+        //         [first_child_coord.x * (max_width / width), first_child_coord.y * (max_height / height)]
+        //     ]:
+        //     [
+        //         [this_coord.y * (max_width / height), this_coord.x * (max_height / width)],
+        //         [first_child_coord.y * (max_width / height), first_child_coord.x * (max_height / width)]
+        //     ]
+        //     return link_coords
+        // })
     }
 
     update_links(translation_scaling) {
         const svg = d3.select(`#${this.svgId}`);
+        // const transform_scaling = transform === undefined? 1 : transform.k
+        const transform_scaling = 1;
         svg.select("g.links")
             .selectAll("path.link")
             .data(this.dag.links(), ({source, target}) => `${source.data.id}-${target.data.id}`)
@@ -222,28 +197,19 @@ export class DAG {
             .attr("class", "link")
             // .attr("d", (d) => this.line([node_positions[d.source.data.id], node_positions[d.target.data.id]]))
             // .attr("d", ({points}) => this.line(points.map(p => { return {x: p.x * translation_scaling[0], y: p.y * translation_scaling[1]}})))
-            .attr("d", ({points}) => this.line(points.map(p => [p[0] * translation_scaling[0], p[1] * translation_scaling[1]])))
+            .attr("d", ({points}) => this.line(points.map(p => [p[0] * translation_scaling[0] / transform_scaling, p[1] * translation_scaling[1] / transform_scaling])))
             .attr("fill", "none")
             .attr("stroke-width", 3)
             .attr("stroke", "gray")
     }
 
     zoomed(e, self) {
+        console.log("zoomed", e.transform)
         const svg = d3.select(`#${self.svgId}`);
         svg.select("g.nodes").attr("transform", `translate(${e.transform.x}, ${e.transform.y}) scale(${e.transform.k})`);
         svg.select("g.links").attr("transform", `translate(${e.transform.x}, ${e.transform.y}) scale(${e.transform.k})`);
         document.querySelectorAll(self.selection_card).forEach(div => {
             applyTransform(div, e.transform)
-            // div.style.transform = `scale(${e.transform.k})`
-            // div.style.scale = e.transform.k
-            // const left = parseFloat(div.style.left)
-            // const top = parseFloat(div.style.top)
-            // const width = div.getBoundingClientRect().width
-            // const height = div.getBoundingClientRect().height
-            // const offsetX = left + width / 2 / e.transform.k
-            // const offsetY = top + height / 2 / e.transform.k
-            // const transform = `translate(${-offsetX}px, ${-offsetY}px) scale(${e.transform.k}) translate(${offsetX}px, ${offsetY}px) translate(${e.transform.x / e.transform.k}px, ${e.transform.y / e.transform.k}px)`
-            // div.style.transform = transform
         })
       }
 }
@@ -301,6 +267,7 @@ function create_bubble_path(points, radius) {
   }
 
   function applyTransform(div, transform) {
+    const id = div.dataset.id
     const left = parseFloat(div.style.left)
     const top = parseFloat(div.style.top)
     const width = div.getBoundingClientRect().width * (div.style.transform === ""? transform.k : 1)
